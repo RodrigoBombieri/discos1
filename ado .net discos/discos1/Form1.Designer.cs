33aee46dfd0c217c014568f8432666b996e5f502
Rodrigo <rodrigosbombieri@gmail.com>
2023-04-06 18:39:30 -0300

    Centralizar base de datos

--- ado .net discos/discos1/Form1.Designer.cs
+++ ado .net discos/discos1/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1175, 439);
+            this.ClientSize = new System.Drawing.Size(1171, 470);
             this.Controls.Add(this.pibDiscos);
             this.Controls.Add(this.dgvDiscos);
             this.Name = "FormDiscos";
